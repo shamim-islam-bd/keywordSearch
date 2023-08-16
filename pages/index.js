12f72f65c4bbb0839698993042e6f93689155ed7
shamim-islam-bd <shamimislam5091@gmail.com>
2023-08-16 13:30:49 +0600

    added

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,98 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
 
 export default function Home() {
+  const { register, handleSubmit, formState: { isSubmitting }, setError } = useForm();
+  const [apiResponse, setApiResponse] = React.useState(null);
+
+  const fetchData = async (data) => {
+    try {
+      const response = await axios.get('https://ultimate-keywords-production.up.railway.app/keyword/', {
+        params: {
+          keyword: data.keyword,
+          lang: data.lang,
+          country: data.country,
+          domain: data.domain,
+        },
+      });
+      setApiResponse(response.data);
+
+    } catch (error) {
+      setError('keyword', { type: 'manual', message: 'Error fetching data' });
+    }
+  };
+
+  const onSubmit = async (data) => {
+    fetchData(data);
+  };
+
+  console.log("apiResponse", apiResponse);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+
+<div className="bg-gray-100 min-h-screen py-8">
+  <div className="max-w-6xl mx-auto px-4">
+    <h1 className="text-3xl font-semibold mb-6 text-center">NextJS 13 Keyword Search</h1>
+    <form onSubmit={handleSubmit(onSubmit)} className="bg-white p-6 rounded shadow-md">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mb-6">
+        <input {...register('keyword', { required: true })} type="text" placeholder="Keyword" className="border p-2 rounded focus:outline-none focus:border-blue-500" />
+        <select {...register("lang")} className="border p-2 rounded focus:outline-none focus:border-blue-500">
+          <option value="english">English</option>
+          <option value="spanish">Spanish</option>
+          <option value="hindi">Hindi</option>
+        </select>
+        <select {...register("country")} className="border p-2 rounded focus:outline-none focus:border-blue-500">
+          <option value="USA">USA</option>
+          <option value="Canada">Canada</option>
+          <option value="Brazil">Brazil</option>
+        </select>
+        <select {...register("domain")} className="border p-2 rounded focus:outline-none focus:border-blue-500">
+          <option value="Google">Google</option>
+          <option value="facebook">facebook</option>
+          <option value="instram">instram</option>
+        </select>
+      </div>
+      <button type="submit" disabled={isSubmitting} className={`bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-600 transition duration-300 ease-in-out ${isSubmitting ? 'opacity-50 cursor-not-allowed' : ''}`}>
+        {isSubmitting ? 'Searching...' : 'Submit'}
+      </button>
+    </form>
+    {apiResponse && (
+      <div className="mt-8">
+        <div className="overflow-x-auto">
+          <table className="w-full table-auto bg-white rounded shadow-md">
+            <thead>
+              <tr className="bg-gray-200 text-left">
+                <th className="p-4">Keyword</th>
+                <th className="p-4">Competition</th>
+                <th className="p-4">Volume</th>
+                <th className="p-4">CPC Curr</th>
+                <th className="p-4">CPC Value</th>
+                <th className="p-4">Auto Complete</th>
+                <th className="p-4">Related Searches</th>
+                <th className="p-4">Related Questions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="border-t">
+                <td className="p-4">{apiResponse.keyword}</td>
+                <td className="p-4">{apiResponse.competition}</td>
+                <td className="p-4">{apiResponse.volume}</td>
+                <td className="p-4">{apiResponse.cpc_currency}</td>
+                <td className="p-4">{apiResponse.cpc_value}</td>
+                <td className="p-4">{apiResponse.auto_complete.map((item, index) => <div key={index}>{item}</div>)}</td>
+                <td className="p-4">{apiResponse.related_searches.map((item, index) => <div key={index}>{item}</div>)}</td>
+                <td className="p-4">{apiResponse.related_questions.map((item, index) => <div key={index}>{item}</div>)}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
+    )}
+  </div>
+</div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
